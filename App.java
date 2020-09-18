ad132e4e5de4c25d8ac2e6c07fc1175a46f8e8c5
JeonSaEun <66246512+JeonSaEun@users.noreply.github.com>
2020-09-18 16:53:54 +0900

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+package com.zerock.ex02;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Hello world!
+ *
+ */
+
+@Log4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        log.info( "Hello World!" );
+    }
+}
